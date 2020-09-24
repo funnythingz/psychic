e3a6f3a97f6cf282b3f0d6336bfe1a1af78aa203
funnythingz <funnythingz69+github@gmail.com>
2020-09-24 23:24:20 +0900

    added logic

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 .container
-  section
+  section(v-if="next")
     p.description
       | 1日1回限定! 感覚を研ぎ澄ませ!
     h1
@@ -10,16 +10,40 @@
     p.stage-title
       | {{content}}の画像が入っている箱を選べ!
     .box-container
-      .box
-      .box
+      .box(@click="lottery(0)")
+      .box(@click="lottery(1)")
+  section(v-else)
+    p.goodbye
+      | おわりだよ！またね！
+    p.reset(@click="reset()")
+      | 最初からやりなおす
 </template>
 
 <script>
+import shuffle from '../lib/shuffle.js'
+
 export default {
   data() {
     return {
+      next: true,
       stage: 1,
       content: '犬',
+    }
+  },
+  methods: {
+    lottery(selectNum){
+      if (shuffle([0, 1])[0] === selectNum) {
+        this.stage += 1;
+        alert('Win!')
+      } else {
+        alert('Lose!')
+        this.next = false
+      }
+    },
+    reset(){
+      this.next = true
+      this.stage = 1
+      this.content = '犬'
     }
   }
 }
@@ -48,4 +72,9 @@ h1
   display: block
   margin: 10px 20px
   background-image: url('~/assets/images/box.png')
+.goodbye
+  font-size: 18px
+.reset
+  font-weight: bold
+  text-decoration: underline
 </style>
