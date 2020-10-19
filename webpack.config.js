466d5a84e65e9dfa7ad9bc1aa166d20f4cae9c5e
funnythingz <funnythingz69+github@gmail.com>
2020-10-19 23:54:21 +0900

    Fixed indent

--- webpack.config.js
+++ webpack.config.js
@@ -7,102 +7,102 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 let config = {
 
-    entry: {
-        app: [
-            path.resolve(__dirname, 'src/app.js')
-        ],
-        styles: [
-            path.resolve(__dirname, 'assets/sass/styles.sass')
-        ],
-        images: [
-            path.resolve(__dirname, 'assets/images/description.png'),
-            path.resolve(__dirname, 'assets/images/logo.png'),
-            path.resolve(__dirname, 'assets/images/card1.png'),
-            path.resolve(__dirname, 'assets/images/card2.png')
-        ]
-    },
-
-    output: {
-        filename: '[name]-[hash].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader",
-                        options: { minimize: true }
-                    }
-                ]
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
-                test: /\.sass$/,
-                use: [
-                    'vue-style-loader',
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            indentedSyntax: true
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(jpg|png|gif)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath : 'assets/images/',
-                            publicPath : function(path){
-                                return '../' + path;
-                            }
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            },
-            {
-                test: /\.pug$/,
-                oneOf: [
-                    {
-                        resourceQuery: /^\?vue/,
-                        use: ['pug-plain-loader']
-                    }
-                ]
-            }
-        ]
-    },
-
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new VueLoaderPlugin(),
-        new HtmlWebPackPlugin({
-            template: "./src/index.html",
-            filename: "./index.html"
-        })
+  entry: {
+    app: [
+      path.resolve(__dirname, 'src/app.js')
+    ],
+    styles: [
+      path.resolve(__dirname, 'assets/sass/styles.sass')
+    ],
+    images: [
+      path.resolve(__dirname, 'assets/images/description.png'),
+      path.resolve(__dirname, 'assets/images/logo.png'),
+      path.resolve(__dirname, 'assets/images/card1.png'),
+      path.resolve(__dirname, 'assets/images/card2.png')
     ]
+  },
+
+  output: {
+    filename: '[name]-[hash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          'vue-style-loader',
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              indentedSyntax: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath : 'assets/images/',
+              publicPath : function(path){
+                return '../' + path;
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.pug$/,
+        oneOf: [
+          {
+            resourceQuery: /^\?vue/,
+            use: ['pug-plain-loader']
+          }
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new VueLoaderPlugin(),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
 }
 
 module.exports = (env) => {
-    return config;
+  return config;
 }
